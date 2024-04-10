eb3a79e0d463d76565439583e3270b4121ac9e5a
互联网高级技工 <2609482146@qq.com>
2024-04-10 10:56:13 +0800

    init

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,23 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local Util = require("lazyvim.util")
+
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  -- 如果存在懒按键处理器就不要创建按键映射？
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
+-- better up/down
+map({ "i", "v" }, "jk", "<esc>", { silent = true, desc = "exit insert" })
+
+map({ "n", "v" }, "H", "^", { silent = true, desc = "line left" })
+map({ "n", "v" }, "L", "$", { silent = true, desc = "line right" })
